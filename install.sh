28cfa0f8de53174dc3ff5ef6188787a56b6ac9ed
herove <herove@gmail.com>
2015-10-23 12:17:55 +0800

    lzsz

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
 ln -s vimrc ~/.vimrc
 ln -s .zshrc ~/.zshrc
+ln -s .gitconfig ~/.gitconfig
+ln -s ~/dotfiles/iterm2/iterm2-send-zmodem.sh /usr/local/bin/iterm2-send-zmodem.sh
+ln -s ~/dotfiles/iterm2/iterm2-recv-zmodem.sh /usr/local/bin/iterm2-recv-zmodem.sh
